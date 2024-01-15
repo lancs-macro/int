4a56268246855914a39cb4ee62d5a25a301ba229
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2024-01-15 14:09:04 +0200

    add report structure

---
+++ reports/parsing_reports.R
@@ -0,0 +1,14 @@
+library(readr)
+library(whisker)
+
+# read plain file
+report <- rvest::read_html("2024q1/report.html")
+
+# reports <- read_file("2024q1/report.html")
+base <- read_file('base.html')
+
+result <- whisker.render(base, list(content = report))
+
+write_lines(result, "report2024q1.html")
+
+
